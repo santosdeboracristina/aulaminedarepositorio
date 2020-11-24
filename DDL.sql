a1ad3c65058167ff479d07b607d0a4c37597a353
santosdeboracristina <santosdeboracristina@hotmail.com>
2020-11-24 00:06:57 +0000

    Adicionando Livro

--- DDL.sql
+++ DDL.sql
@@ -21,6 +21,13 @@ create table aut_autorizacao (
   unique key uni_aut_nome (aut_nome)
 );
 
+create table liv_livros (
+  liv_id bigint unsigned not null auto_increment,
+  liv_titulo varchar(20) not null,
+  primary key (liv_id),
+  unique key uni_liv_titulo (liv_titulo)
+);
+
 create table uau_usuario_autorizacao (
   usr_id bigint unsigned not null,
   aut_id bigint unsigned not null,
@@ -29,8 +36,19 @@ create table uau_usuario_autorizacao (
   foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
 );
 
+create table uau_usuario_livro (
+  usr_id bigint unsigned not null,
+  liv_id bigint unsigned not null,
+  primary key (usr_id, liv_id),
+  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+  foreign key liv_livrod_fk (aut_id) references liv_livros (liv_id) on delete restrict on update cascade
+);
+
 insert into usr_usuario (usr_nome, usr_senha)
     values ('Debora', 'SenhaFr4ca');
 insert into aut_autorizacao (aut_nome)
     values ('ROLE_ADMIN');
+insert into liv_livros (liv_titulo)
+    values ('Anne_of_Green_Gables');
 insert into uau_usuario_autorizacao values (1,1);
+insert into uau_usuario_livro values (1,1);
