61d7ba0cf50721d945f3cc78a9c9ee25f6258f80
santosdeboracristina <santosdeboracristina@hotmail.com>
2021-01-27 16:33:44 +0000

    Adicionando UserDetails

--- src/main/java/br/gov/sp/fatec/springbootapp/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package br.gov.sp.fatec.springbootapp.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 
-public interface UsuarioService {
+public interface UsuarioService extends UserDetailsService {
 
     public Usuario novoUsuario(String nome, String email, String senha, String nomeAutorizacao);
 
--- src/main/java/br/gov/sp/fatec/springbootapp/service/UsuarioServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/UsuarioServiceImpl.java
@@ -1,12 +1,14 @@
 package br.gov.sp.fatec.springbootapp.service;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
@@ -39,6 +41,22 @@ public class UsuarioServiceImpl implements UsuarioService {
         usuario.getAutorizacoes().add(autorizacao);
         usuarioRepo.save(usuario);
         return usuario;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) 
+    throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepo.findTop1ByNomeOrEmail(username, username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário " + username + " não encontrado");
+        }
+        return User.builder().username(username)
+        .password(usuario.getSenha())
+        .authorities(usuario.getAutorizacoes()
+        .stream()
+        .map(Autorizacao::getNome)
+        .collect(Collectors.toList())
+        .toArray(new String[usuario.getAutorizacoes().size()])).build();
     }
 
 }
